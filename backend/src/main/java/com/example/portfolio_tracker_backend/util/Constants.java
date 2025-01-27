c7cbe41875ab8b7fc34de5d5611b96061aa0c9e0
praneeth <lakshmipraneeth1461@gmail.com>
2025-01-28 00:01:58 +0530

    backend done

---
+++ backend/src/main/java/com/example/portfolio_tracker_backend/util/Constants.java
@@ -0,0 +1,8 @@
+package com.example.portfolio_tracker_backend.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Constants {
+    public static final List<String> AVAILABLE_TICKERS = new ArrayList<>(List.of("AAPL", "MSFT", "GOOGL", "AMZN", "TSLA"));
+}
